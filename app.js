4adedf589a69cd6cdcf6ba0525c8b7dd61dd14a9
Mella <melladjs@hotmail.com>
2021-06-16 09:43:44 +0200

    instalacion mysql y dotenv

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const watchesRouter = require('./routes/watches');
+const apiRouter = require('./routes/api');
 
 var app = express();
+
+// Configuración fichero .env
+require('dotenv').config();
+
+// Configuración base de datos
+require('./dbConfig');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/watches', watchesRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/watches.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+// GET http://localhost:3000/watches
+
+
+module.exports = router;
