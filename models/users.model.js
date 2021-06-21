c2796708f0f3c001582bf169b775cebfd0b36b32
Mella <melladjs@hotmail.com>
2021-06-21 16:43:07 +0200

    registro y login funcionando en node

--- models/users.model.js
+++ models/users.model.js
@@ -1,17 +1,6 @@
-const { deleteById } = require("./foro.model")
-
-const getAll = () => {
-    return new Promise((resolve, reject) => {
-        deleteById.query('SELECT * FROM heroku_bf6019206e7eabe.users', (err, rows) => {
-            if (err) reject(err)
-            resolve(rows);
-        });
-    })
-}
-
 const createClient = ({ nombre, apellidos, direccion, sexo, fecha_nacimiento, email, password, repite_password }) => {
     return new Promise((resolve, reject) => {
-        db.query('INSERT INTO users (nombre, apellidos, direccion, sexo, fecha_nacimiento, email, password, repite_password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, )', [nombre, apellidos, direccion, sexo, fecha_nacimiento, email, password, repite_password], (err, result) => {
+        db.query('INSERT INTO heroku_bf6019206e7eabe.users (nombre, apellidos, direccion, sexo, fecha_nacimiento, email, password, repite_password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [nombre, apellidos, direccion, sexo, fecha_nacimiento, email, password, repite_password], (err, result) => {
             if (err) reject(err)
             if (result) {
                 resolve(result)
@@ -22,13 +11,22 @@ const createClient = ({ nombre, apellidos, direccion, sexo, fecha_nacimiento, em
 
 const getByEmail = (pEmail) => {
     return new Promise((resolve, reject) => {
-        db.query(`SELECT * FROM heroku_bf6019206e7eabe.nuevo_post where email="${pEmail}"`, (err, rows) => {
+        db.query(`SELECT * FROM heroku_bf6019206e7eabe.users where email="${pEmail}"`, (err, rows) => {
             if (err) reject(err);
             resolve(rows[0]);
         })
     })
 }
 
+const getById = (pId) => {
+    return new Promise((resolve, reject) => {
+        db.query(`SELECT * FROM heroku_bf6019206e7eabe.users where id="${pId}"`, (err, rows) => {
+            if (err) reject(err);
+            resolve(rows[0]);
+        });
+    });
+}
+
 module.exports = {
-    getAll, createClient, getByEmail
+    createClient, getByEmail, getById
 }
--- models/watches.model.js
+++ models/watches.model.js
@@ -19,8 +19,7 @@ const getByMarca = (pWatchBrand) => {
 const create = ({ watch_brand, watch_model, release_year, market_price, retail_price, owner, availability, notes, box_included, assignment_date, features, family, movement, purchase_year, limited, material, diameter, dial_color, imagen }) => {
     return new Promise((resolve, reject) => {
         db.query(
-            'insert into watches (watch_brand, watch_model, release_year, market_price, retail_price, owner, availability, notes, box_included, assignment_date, features, family, movement,purchase_year, limited, material, diameter, dial_color, imagen) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)',
-            [watch_brand, watch_model, release_year, market_price, retail_price, owner, availability, notes, box_included, assignment_date, features, family, movement, purchase_year, limited, material, diameter, dial_color, imagen],
+            'insert into watches (watch_brand, watch_model, release_year, market_price, retail_price, owner, availability, notes, box_included, assignment_date, features, family, movement,purchase_year, limited, material, diameter, dial_color, imagen) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)', [watch_brand, watch_model, release_year, market_price, retail_price, owner, availability, notes, box_included, assignment_date, features, family, movement, purchase_year, limited, material, diameter, dial_color, imagen],
             (err, result) => {
                 if (err) reject(err);
                 resolve(result);
